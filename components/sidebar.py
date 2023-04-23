06ab8bfccdb55bf95504a4b85312b0cfedcfac6d
Abner Santos <abner.santos@bdsdatasolution.com.br>
2023-04-23 20:09:30 -0300

    Adicionado Função do botão receita e despesa

--- components/sidebar.py
+++ components/sidebar.py
@@ -39,7 +39,23 @@ layout = dbc.Col([
                     ], width=6)        
                 ]),
 
-# Seção NAV
+# Model Receita ------------------------------------------
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adicionar receita')),
+                    dbc.ModalBody([
+    
+                    ])
+                ], id='modal-novo-receita'),
+
+# Model Despesa ------------------------------------------
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adicionar despesa')),
+                    dbc.ModalBody([
+    
+                    ])
+                ], id='modal-novo-despesa'),
+
+# Seção NAV ----------------------------------------------
 
                 html.Hr(),
                 dbc.Nav(
@@ -54,3 +70,21 @@ layout = dbc.Col([
 
 # =========  Callbacks  =========== #
 # Pop-up receita
+@app.callback(
+    Output('modal-novo-receita','is_open'),
+    Input('open-novo-receita', 'n_clicks'),
+    State('modal-novo-receita','is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
+    
+# Pop-up despesa
+@app.callback(
+    Output('modal-novo-despesa','is_open'),
+    Input('open-novo-despesa', 'n_clicks'),
+    State('modal-novo-despesa','is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
